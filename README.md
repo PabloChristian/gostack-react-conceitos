c3a8cd40686aae5a46e73e3e45e11883e6b7c554
Pablo Christian <pablo@linearsistemas.com.br>
2021-04-04 22:59:40 -0300

    aula do dia 04-04-2021

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gostack-react-conceitos
 Repositório contendo a implementação de conceitos de React do curso GoStack da RocketSeat, abrangendo conceitos de configuração Babel, WebPack, Componentização.
+
+## Iniciando a Aplicação
+
+Será inicializado pelo Webpack, digitando o comando abaixo:
+yarn dev
+esse comando irá por trás dos panos executar o comando 'yarn webpack serve --mode development'
--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React , { useState, useEffect } from 'react';
 import Header from './components/Header';
+import './App.css';
+import backgroundImage from './assets/background.jpeg';
+import api from './services/api';
 
 function App() {
-  return <Header title="Homepage">
-            <ul>
-              <li>Homepage</li>
-              <li>Projects</li> 
-            </ul>
-          </Header>;
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      console.log(response);
+    })
+  }, []);
+
+  function handleAddProject() {
+    // utilizamos spread operator para criar um novo array
+    setProjects([...projects, `Novo projeto ${Date.now()}`]) 
+    console.log(projects);
+  }
+
+  return (
+    <>
+      <Header title="Projects" />
+      <img width={300} src={backgroundImage}/>
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+    </>
+  );
 }
 
 export default App;
